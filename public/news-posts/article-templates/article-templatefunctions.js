a95e0416ee645a4599b075c1b6ce2e4555856e5e
RealSyncro <loongguicheng@gmail.com>
2022-07-20 09:12:57 +0800

    Javascript so fun

---
+++ public/news-posts/article-templates/article-templatefunctions.js
@@ -0,0 +1,25 @@
+// V0.1 (Prototype)
+console.log ("Gui Cheng's Script");
+alert ('JS enabled.')
+
+let newsSelector = [
+{News_Name: 'News1', 
+ Desciption: 'This is a sample desciption.'
+}
+{News_Name: 'News2',
+ Description: 'This is a sample description2.'
+
+}
+]
+
+function sliderBanner(){
+}   
+
+function productsSlider(){
+}
+
+function sliderNewsCard(){
+}
+
+function starRating(){
+}
